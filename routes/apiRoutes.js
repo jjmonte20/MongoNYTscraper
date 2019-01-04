710a5024cce8b28bf5160c885c54a15143b4f118
jjmonte20 <jjmonte20@gmail.com>
2019-01-04 12:12:15 -0600

    added some handlebars files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+var db = require("../models");
+
+// need express
+var express = require("express");
+
+// need to route as well
+var router = express.Router();
+
+// need to have an api route for all of the scraped articles
+
+// also need to have one for when the user saves an article
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,23 @@
+var db = require("../models");
+
+// need express
+var express = require("express");
+
+// need to route
+var router = express.Router();
+
+// load the index page
+router.get("/", function(req, res) {
+    res.render("index", {
+        title: "ShoryuScraper",
+        msg: "Welcome"
+    });
+});
+
+// load the saved page
+router.get("/saved", function(req, res) {
+    res.render("saved", {
+        title: "Saved Articles",
+        msg: "Here are your saved articles"
+    });
+});
